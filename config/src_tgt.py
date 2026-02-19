4b1a553294e7c3597f0a1f8f95d3e1c667bcb7a8
Suresha <rdssuri403@gmail.com>
2026-02-19 14:18:27 +0530

    final secret injection fix

--- config/src_tgt.py
+++ config/src_tgt.py
@@ -4,22 +4,29 @@ import os
 
 
 def source_csv():
-    df = pd.read_csv(r"./data/customer.csv",nrows= 1009)
+    df = pd.read_csv(r"./data/customer.csv", nrows=1009)
     return df
-
 
 
 def snowflake_conn():
 
+    user = os.getenv('SF_USER')
+    password = os.getenv('SF_PASSWORD')
+    account = os.getenv('SF_ACCOUNT')
+    warehouse = os.getenv('SF_WAREHOUSE')
+    database = os.getenv('SF_DATABASE')
+    schema = os.getenv('SF_SCHEMA')
+
+    if not user:
+        raise Exception("SF_USER not found. GitHub Secrets not injected!")
+
     conn = snowflake.connector.connect(
-        user=os.environ['SF_USER'],
-        password=os.environ['SF_PASSWORD'],
-        account=os.environ['SF_ACCOUNT'],
-        warehouse=os.environ['SF_WAREHOUSE'],
-        database=os.environ['SF_DATABASE'],
-        schema=os.environ['SF_SCHEMA']
+        user=user,
+        password=password,
+        account=account,
+        warehouse=warehouse,
+        database=database,
+        schema=schema
     )
 
     return conn
-
-
